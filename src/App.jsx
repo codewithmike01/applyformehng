bdc67be2b066459fd2a288936c08492470b473db
ginohmk <michotall95@gmail.com>
2022-11-21 07:35:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing_page/LandingPage";
+import AboutUs from "./pages/about_us/AboutUs";
+import FAQs from "./pages/faqs/FAQs";
+import ContactUs from "./pages/contact_us/ContactUs";
+import TermsAndCondition from "./pages/terms_condition/TermsAndCondition";
+import Privacy from "./pages/privacy/Privacy";
+import Cookies from "./pages/cookies/Cookies";
+import Career from "./pages/career/Career";
+import Blog from "./pages/blog/Blog";
+import PricingPlan from "./pages/pricing_plan/PricingPlan";
+import Error from "./pages/error/Error";
+import Dashboard from "./pages/dashboard/Dashboard";
+import UserDashboardLayout from "./pages/user_dashboard/UserDashboardLayout";
+import DashboardNothing from "./pages/dashboard_noting/DashboardNothing";
+
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route exact path="/" element={<LandingPage />} />
+                <Route exact path="/about" element={<AboutUs />} />
+                <Route exact path="/faqs" element={<FAQs />} />
+                <Route exact path="/contact" element={<ContactUs />} />
+                <Route exact path="/t&c" element={<TermsAndCondition />} />
+                <Route exact path="/privacy" element={<Privacy />} />
+                <Route exact path="/cookies" element={<Cookies />} />
+                <Route exact path="/career" element={<Career />} />
+                <Route exact path="/pricing" element={<PricingPlan />} />
+                <Route exact path="/user-page" element={<Dashboard />} />
+                <Route exact path="/blog/:id" element={<Blog />} />
+                <Route path="dashboard" element={<UserDashboardLayout />}>
+                    <Route path="/dashboard/" element={<DashboardNothing />} />
+                </Route>
+
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
